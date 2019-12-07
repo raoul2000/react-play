cf66e3db39c268d60c95dd7d10725673b77d23fb
raoul2000 <manu34@free.fr>
2019-12-07 15:05:04 +0100

    add todo stats info in toolbar

--- src/components/todo-widget/Toolbar.js
+++ src/components/todo-widget/Toolbar.js
@@ -3,17 +3,26 @@ import { connect } from 'react-redux';
 import propTypes from 'prop-types';
 import { fetchTodoList } from './actions';
 
-const Toolbar = ({ onRefreshTodoList }) => {
+const Toolbar = ({ onRefreshTodoList, todoItemCount = 0, completedTodoItemCount = 0 }) => {
     return (
         <div className="toolbar">
             <button onClick={onRefreshTodoList}>refresh</button>
+            <span className="stat-info">
+                total : {todoItemCount} | completed : {completedTodoItemCount} | uncompleted : {todoItemCount - completedTodoItemCount}
+            </span>
         </div>
     );
 };
 
 Toolbar.propTypes = {
-    onRefreshTodoList: propTypes.func.isRequired
+    onRefreshTodoList: propTypes.func.isRequired,
+    todoItemCount: propTypes.number
 };
+
+const mapStateToProps = (state) => ({
+    todoItemCount: state.todoList.length,
+    completedTodoItemCount: state.todoList.filter(todoItem => todoItem.completed).length
+});
 
 const mapDispatchToProps = dispatch => {
     return {
@@ -23,4 +32,4 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-export default connect(null, mapDispatchToProps)(Toolbar);
+export default connect(mapStateToProps, mapDispatchToProps)(Toolbar);
